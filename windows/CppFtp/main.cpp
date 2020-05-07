e3e6e36868889bf092afe3ea0fad67c4bcd2b11f
yw.lee <yw.lee@ivpl.sookmyung.ac.kr>
2020-05-07 21:29:30 +0900

    main.cpp 소스 UTF-8 인코딩 에러 발생

--- windows/CppFtp/main.cpp
+++ windows/CppFtp/main.cpp
@@ -4,28 +4,35 @@
 #include <iostream>
 #pragma comment(lib, "wininet")
 
-int main() {
-    CString ip = L"";
-    CString id = L"";
-    CString pw = L"";
+CString ip = L"eileenyoo.cafe24.com";
+CString id = L"eileenyoo";
+CString pw = L"sapere4ude";
 
-    CString src_path = L"C:\\Temp\\input2.txt"; // 전송하고자 하는 파일의 경로
-    CString dest_fname = L"MyFtpTest.txt";      // 서버에 저장될 파일 이름
+int send_file(CString src_path, CString fname) {
 
     HINTERNET hInternet;
     HINTERNET hFtpSession;
     hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
     hFtpSession = InternetConnect(hInternet, ip, INTERNET_DEFAULT_FTP_PORT, id, pw, INTERNET_SERVICE_FTP, 0, 0);
     if (hFtpSession) {
-        if (FtpPutFile(hFtpSession, src_path, dest_fname, FTP_TRANSFER_TYPE_BINARY, 0)) {
-            std::cout << "성공적으로 업로드 완료됨" << std::endl;
+        if (FtpPutFile(hFtpSession, src_path, fname, FTP_TRANSFER_TYPE_BINARY, 0)) {
+            std::cout << "Successfully uploaded to FTP Server" << std::endl;
         }
         else {
-            std::cout << "업로드 실패" << std::endl;
+            std::cout << "Failed" << std::endl;
         }
     }
     InternetCloseHandle(hFtpSession);
     InternetCloseHandle(hInternet);
+
+    return 0;
+}
+
+int main() {
+    CString src_path = L"C:\\Temp\\test.txt";
+    CString fname = L"ABCD.txt";
+    
+    send_file(src_path, fname);
 
     return 0;
 }
